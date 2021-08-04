ba1627446acd1d55db5fd9d5577fd405083d2de7
root <tnghgks@naver.com>
2021-08-04 15:56:18 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Message from "Components/Message";
+
+class App extends React.Component {
+  render() {
+    return <Message></Message>
+  }
+}
+
+export default App;
